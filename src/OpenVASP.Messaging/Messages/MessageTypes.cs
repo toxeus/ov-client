b6c877b55f667e99a9ccc42b65853ddaeede0cb8
Nika Gamkrelidze <nikoloz.gamkrelidze@lykke-business.ch>
2020-04-24 14:50:15 +0400

    message type formatting fixed.

--- src/OpenVASP.Messaging/Messages/MessageTypes.cs
+++ src/OpenVASP.Messaging/Messages/MessageTypes.cs
@@ -1,19 +1,28 @@
-﻿namespace OpenVASP.Messaging.Messages
+﻿using System.Runtime.Serialization;
+
+namespace OpenVASP.Messaging.Messages
 {
     public enum MessageType
     {
+        [EnumMember(Value = "110")]
         SessionRequest = 110,
 
+        [EnumMember(Value = "150")]
         SessionReply = 150,
 
+        [EnumMember(Value = "210")]
         TransferRequest = 210,
 
+        [EnumMember(Value = "250")]
         TransferReply = 250,
 
+        [EnumMember(Value = "310")]
         TransferDispatch = 310,
 
+        [EnumMember(Value = "350")]
         TransferConfirmation = 350,
 
+        [EnumMember(Value = "910")]
         Termination = 910,
     }
 }
