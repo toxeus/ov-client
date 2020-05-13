8d1e6130fe2a9030d694d6ba738785f32b677aea
Nika Gamkrelidze <nikoloz.gamkrelidze@lykke-business.ch>
2020-05-13 21:59:30 +0400

    transfer reply extended.

--- src/OpenVASP.Messaging/Messages/TransferReplyMessage.cs
+++ src/OpenVASP.Messaging/Messages/TransferReplyMessage.cs
@@ -7,21 +7,27 @@ namespace OpenVASP.Messaging.Messages
 {
     public class TransferReplyMessage : MessageBase
     {
-        public static TransferReplyMessage Create(Message message)
+        public static TransferReplyMessage Create(Message message, string destinationAddress)
         {
             return new TransferReplyMessage
             {
-                Message = message
+                Message = message,
+                DestinationAddress = destinationAddress
             };
         }
 
-        public static TransferReplyMessage Create(string sessionId,  TransferReplyMessageCode transferReplyMessageCode)
+        public static TransferReplyMessage Create(
+            string sessionId,  TransferReplyMessageCode transferReplyMessageCode, string destinationAddress)
         {
             return new TransferReplyMessage
             {
-                Message = new Message(Guid.NewGuid().ToByteArray().ToHex(true), sessionId, GetMessageCode(transferReplyMessageCode), MessageType.TransferReply)
+                Message = new Message(Guid.NewGuid().ToByteArray().ToHex(true), sessionId, GetMessageCode(transferReplyMessageCode), MessageType.TransferReply),
+                DestinationAddress = destinationAddress
             };
         }
+        
+        [JsonProperty("destinationAddr")]
+        public string DestinationAddress { get; private set; }
 
         public static string GetMessageCode(TransferReplyMessageCode messageCode)
         {
